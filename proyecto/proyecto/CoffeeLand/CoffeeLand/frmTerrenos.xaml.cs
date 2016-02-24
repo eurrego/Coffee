abc386025dfc1f932716f5595b8d3636362c4efa
eurrego <eurrego2@misena.edu.co>
2016-02-24 10:38:23 -0500

    avance sobre vista, correción de bugs

--- proyecto/proyecto/CoffeeLand/CoffeeLand/frmTerrenos.xaml.cs
+++ proyecto/proyecto/CoffeeLand/CoffeeLand/frmTerrenos.xaml.cs
@@ -17,6 +17,7 @@ using System.Collections;
 using System.Globalization;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using System.Data;
 
 namespace CoffeeLand
 {
@@ -40,18 +41,31 @@ namespace CoffeeLand
 
         #endregion
 
+        public static DataTable dt;
+        public static DataTable dt1;
+
+        int init = 0;
+        int init1 = 0;
+        int opc = 0;
+        int opc2 = 0;
+        int index = -1;
+        int cantidadArboles = 0;
+
+        bool validacion = false;
 
         public frmTerrenos()
         {
             InitializeComponent();
             mostrar();
             tamanioPantalla();
+            dtdFechaLabor.DisplayDateEnd = DateTime.Now;
         }
 
         public void mostrar()
         {
             tblLotes.ItemsSource = MTerrenos.GetInstance().ConsultarLote();
             tblLabores.ItemsSource = MTerrenos.GetInstance().ConsultarLabor();
+            cmbInsumo.ItemsSource = MTerrenos.GetInstance().ConsultarInsumo();
             llenarCmbTipoPago();
 
         }
@@ -77,9 +91,13 @@ namespace CoffeeLand
             var anchoContainer = width / 2.25;
             pnlContainer.Width = anchoContainer;
             pnlContainerLabor.Width = anchoContainer;
+            pnlContainerInsumos.Width = width / 1.75;
 
             tblLotes.Height = height - 285;
+            columnLote.Width = anchoContainer - 70;
             tblLabores.Height = height - 285;
+            columnLabor.Width = anchoContainer - 70;
+            tblInsumos.Height = height - 285;
 
         }
 
@@ -94,6 +112,108 @@ namespace CoffeeLand
             ((MetroWindow)Application.Current.MainWindow).ShowMessageAsync("Información", mensaje);
         }
 
+        private bool Validar(int opc )
+        {
+
+            switch (opc)
+            {
+                case 1:
+                    if (cmbTipoPago.SelectedIndex == 0 || dtdFechaLabor.SelectedDate == null || lblLabores.Text.Equals("Seleccione un"))
+                    {
+                        mensajeError("Debe Ingresar todos los Campos");
+                        validacion = false;
+                    }
+                    else
+                    {
+                        validacion = true;
+                    }
+                    break;
+                case 2:
+                    if (lblLote.Text.Equals("Seleccione un"))
+                    {
+                        mensajeError("Debe seleccionar un lote");
+                        validacion = false;
+                    }
+                    else
+                    {
+                        validacion = true;
+                    }
+                    break;
+                case 3:
+                    if (cmbInsumo.SelectedIndex == 0 || txtCantidadInsumo.Text == string.Empty)
+                    {
+                        mensajeError("Debe Ingresar todos los Campos");
+                        validacion = false;
+                    }
+                    else
+                    {
+                        validacion = true;
+                    }
+
+                    break;
+
+
+                default:
+                    break;
+            }
+
+            return validacion;
+        }
+
+
+        public void limpiarCampos(int opc)
+        {
+            switch (opc)
+            {
+                case 1:
+                    cmbTipoPago.SelectedIndex = 0;
+                    dtdFechaLabor.SelectedDate = null;
+                    lblLabores.Text = "Seleccione un";
+                    lblInicioLabores.Visibility = Visibility.Visible;
+                    tblLabores.SelectedItem = null;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public void crearTabla(int opc2)
+        {
+
+            switch (opc2)
+            {
+                case 1:
+
+                    if (init == 0)
+                    {
+                        dt = new DataTable();
+                        dt.Columns.Add("idLabor_Lote");
+                        dt.Columns.Add("IdInsumo");
+                        dt.Columns.Add("Cantidad");
+                        dt.Columns.Add("Precio");
+                        dt.Columns.Add("NombreInsumo");
+                        init++;
+                    }
+                    break;
+
+                case 2:
+
+                    if (init1 == 0)
+                    {
+                        dt1 = new DataTable();
+                        dt1.Columns.Add("DocumentoPersona");
+                        dt1.Columns.Add("idLabor_Lote");
+                        dt1.Columns.Add("Cantidad");
+                        dt1.Columns.Add("Valor");
+                        dt1.Columns.Add("NombrePersona");
+
+                        init1++;
+                    }
+                    break;
+            }
+        }
+
         private void tblLotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var item = tblLotes.SelectedItem as MTerrenos;
@@ -101,6 +221,7 @@ namespace CoffeeLand
             if (item != null)
             {
                 var tolower = item.NombreLote.ToLower();
+                lblLote.FontFamily = new FontFamily("Arial Rounded MT Bold");
                 lblLote.Text = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(tolower);
                 lblLoteLabores.Text = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(tolower);
 
@@ -121,6 +242,7 @@ namespace CoffeeLand
 
         private void btnAtras_Click(object sender, RoutedEventArgs e)
         {
+            lblLote.FontFamily = new FontFamily("Segoe UI");
             lblLote.Text = "Seleccione un";
             lblInicioLote.Visibility = Visibility.Visible;
             lblTotalArboles.Text = "0";
@@ -131,14 +253,90 @@ namespace CoffeeLand
 
         private void btnSiguiente_Click(object sender, RoutedEventArgs e)
         {
-            if (lblLote.Text.Equals("Seleccione un"))
+            if (Validar(2))
             {
-                mensajeError("Debe seleccionar un lote");
+                tabLabor.Focus();
+                btnLabores.IsChecked = true;             
+            }
+        }
+
+        
+        private void btnSiguienteLabores_Click(object sender, RoutedEventArgs e)
+        {
+            if (Validar(1))
+            {
+                tabInsumo.Focus();
+                btnInsumo.IsChecked = true;
+            }
+        }
+
+        private void btnAtrasLabores_Click(object sender, RoutedEventArgs e)
+        {
+            limpiarCampos(1);
+            tabLote.Focus();
+            btnLabores.IsChecked = false;
+        }
+
+        private void tblLabores_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = tblLabores.SelectedItem as Labor;
+
+            if (item != null)
+            {
+                var tolower = item.NombreLabor.ToLower();
+                lblLabores.Text = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(tolower);
+
+                lblInicioLabores.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void btnAtrasLabor_Click(object sender, RoutedEventArgs e)
+        {
+            limpiarCampos(1);
+        }
+
+        private void cmbInsumo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Insumo insumo = cmbInsumo.SelectedItem as Insumo;
+
+            if (cmbInsumo.SelectedIndex != 0)
+            {
+                var tolower = insumo.UnidadMedida.ToLower();
+                txtUnidadMedida.Text = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(tolower);
+
+                lblStock.Text = insumo.CantidadExistente.ToString();
+                var precioPromedio = insumo.PrecioPromedio;
             }
             else
             {
-                tabLabor.Focus();
+                txtUnidadMedida.Text = string.Empty;
+                lblStock.Text = string.Empty;
             }
+        }
+
+        private void btnAgregarInsumos_Click(object sender, RoutedEventArgs e)
+        {
+            Insumo insumo = cmbInsumo.SelectedItem as Insumo;
+
+
+            if (Validar(3))
+            {
+                crearTabla(1);
+
+                if (insumo.CantidadExistente >= int.Parse(txtCantidadInsumo.Text))
+                {
+                    dt.Rows.Add(null, cmbInsumo.SelectedValue, txtCantidadInsumo.Text, insumo.PrecioPromedio, cmbInsumo.Text);
+
+                    tblInsumos.ItemsSource = dt.DefaultView;
+                }
+                else
+                {
+                    mensajeError("La cantidad del insumo es menor a la ingresada");
+                }
+
+
+            }
+            limpiarCampos(1);
         }
     }
 }
