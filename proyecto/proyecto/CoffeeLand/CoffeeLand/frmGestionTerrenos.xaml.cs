207a128514254acab472061ace7b389829d63de9
devsnug <eurrego2@misena.edu.co>
2016-03-09 21:50:46 -0500

    se implementa el evento selector.selected para instanciar de nuevo el control de usuario en el tabitem

--- proyecto/proyecto/CoffeeLand/CoffeeLand/frmGestionTerrenos.xaml.cs
+++ proyecto/proyecto/CoffeeLand/CoffeeLand/frmGestionTerrenos.xaml.cs
@@ -36,49 +36,25 @@ namespace CoffeeLand
 
         #endregion
 
-        
+
 
         public frmGestionTerrenos()
         {
             InitializeComponent();
             instance = this;
-            
+
         }
 
-        private void tabRegistroTerrenos_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        private void tabArboles_Selected(object sender, RoutedEventArgs e)
+        {
+            frmArboles misArboles = new frmArboles();
+            contentArboles.Content = misArboles;
+        }
+
+        private void TabItem_Selected(object sender, RoutedEventArgs e)
         {
             frmTerrenos misTerrenos = new frmTerrenos();
             contentRegistroTerrenos.Content = misTerrenos;
-        }
-
-        private void TabItem_GotFocus(object sender, RoutedEventArgs e)
-        {
-            tabRegistroTerrenos.Visibility = Visibility.Visible;
-        }
-
-        private void tabArboles_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            
-        }
-
-        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (sender != null)
-            {
-                string tab = (sender as TabItem).Header as string;
-                frmArboles misArboles = new frmArboles();
-
-                switch (tab)
-                {
-                    case "ARBOLES":
-                        contentArboles.Content = misArboles;
-                        break;
-                    default:
-                        break;
-                }
-            }
-          
-
         }
     }
 }
